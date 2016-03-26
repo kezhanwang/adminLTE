ab28b36e4d3e2ea499c1b0344cecdbdf0241f62c
HowieBird <76828528@qq.com>
2016-03-26 15:17:22 +0800

    添加view文件

---
+++ web/protected/views/layouts/header.php
@@ -0,0 +1,82 @@
+<!---header--->
+<div class="header-section">
+    <div class="container">
+        <div class="head-top">
+            <div class="email">
+                <ul>
+                    <li>
+                        <i class="glyphicon glyphicon-log-in" aria-hidden="true"></i>
+                        <a href="#" data-toggle="modal" data-target="#myModal">登录</a>
+                    </li>
+                    <li>
+                        <i class="glyphicon glyphicon-lock" aria-hidden="true"></i>
+                        <a href="#" data-toggle="modal" data-target="#myModal1">注册</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+        <nav class="navbar navbar-default">
+            <!---Brand and toggle get grouped for better mobile display--->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                        data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <div class="navbar-brand">
+                    <h1><a href="index.html"><span>Real </span>Space</a></h1>
+                </div>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="index.html">Home <span class="sr-only">(current)</span></a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                           aria-expanded="false">Pages<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="about.html">About</a></li>
+                            <li><a href="services.html">Services</a></li>
+                            <li><a href="agents.html">Agents</a></li>
+                            <li><a href="forrent.html">For Rent</a></li>
+                            <li><a href="forsale.html">For Sale</a></li>
+                            <li><a href="pricing.html">Pricing</a></li>
+                            <li><a href="faqs.html">FAQs</a></li>
+                            <li><a href="idxpress.html">IDXpress</a></li>
+                            <li><a href="terms.html">Terms of Use</a></li>
+                            <li><a href="privacy.html">Privacy Policy</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown ">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                           aria-expanded="false">Property<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="defaultvariation.html">Default – Variation</a></li>
+                            <li><a href="agentinsidebarvariation.html">Agent in Sidebar - Variation</a></li>
+                            <li><a href="galleryvariation.html">Gallery - Variation</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                           aria-expanded="false">Gallery <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="2columnsgallery.html">2 Columns Gallery</a></li>
+                            <li><a href="3columnsgallery.html">3 Columns Gallery</a></li>
+                            <li><a href="4columnsgallery.html">4 Columns Gallery</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="blog.html">Blog</a></li>
+                    <li><a href="codes.html">Codes</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                </ul>
+
+                <div class="clearfix"></div>
+            </div>
+        </nav>
+    </div>
+</div>
+<!---header--->
