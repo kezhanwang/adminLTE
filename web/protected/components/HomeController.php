4f3f7702e9319c846f0ea1b35ceef140aa041cce
HowieBird <76828528@qq.com>
2016-03-26 12:25:10 +0800

    添加首页的样式和控制器相关内容

---
+++ web/protected/components/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: wangyi
+ * Date: 16/3/26
+ * Time: 12:21
+ */
+class HomeController extends Controller
+{
+    public function __construct()
+    {
+    }
+}
---
+++ web/protected/controllers/IndexController.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: wangyi
+ * Date: 16/3/26
+ * Time: 12:21
+ */
+class IndexController extends HomeController
+{
+
+}
