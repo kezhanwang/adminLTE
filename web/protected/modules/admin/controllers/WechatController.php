d4b1701ff3e32d3df9ad29ccf73b706597f038e7
王毅 <wangyi@wangyideMacBook-Pro.local>
2016-07-26 23:13:01 +0800

    网站管理

---
+++ web/protected/modules/admin/controllers/WechatController.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: wangyi
+ * Date: 16/6/26
+ * Time: 下午2:40
+ */
+class WechatController extends HwAdminController
+{
+    public function actionindex()
+    {
+        $this->render('index');
+    }
+}
---
+++ web/protected/modules/admin/views/wechat/index.php
@@ -0,0 +1,17 @@
+<div class="hold-transition lockscreen" style="height: 500px" xmlns="http://www.w3.org/1999/html">
+    </br>
+    </br>
+    </br>
+    </br>
+    </br>
+    </br>
+    </br>
+    </br>
+    </br>
+    </br>
+    <div class="lockscreen-logo">
+        微信管理模块
+    </div>
+    <!-- User name -->
+    <div class="lockscreen-name"></div>
+</div>
